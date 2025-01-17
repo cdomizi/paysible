9f6a3a38e16c9f3327d456832968bd587ec8fb3d
cdomizi <domizicesare@gmail.com>
2025-01-17 15:26:10 +0100

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Navbar } from "@layout/Navbar";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
---
+++ src/layout/Navbar.tsx
@@ -0,0 +1,11 @@
+export function Navbar() {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1 justify-center">
+        <a className="btn btn-ghost text-xl" href="/">
+          paysible
+        </a>
+      </div>
+    </div>
+  );
+}
