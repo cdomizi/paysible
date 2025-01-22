b297863fc9d7e3ed9fd8403736139be5aca59003
cdomizi <domizicesare@gmail.com>
2025-01-22 14:28:53 +0100

    move logo svg file to public folder

--- src/layout/Logo.tsx
+++ src/layout/Logo.tsx
@@ -2,7 +2,7 @@ export function Logo() {
   return (
     <div>
       <a className="btn btn-ghost text-xl" href="/">
-        <img src="/src/assets/logo.svg" alt="paysible-logo" />
+        <img src="/logo.svg" alt="paysible-logo" />
       </a>
     </div>
   );
