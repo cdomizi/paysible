b9ccbb9fc6bbf8bbc0100dda39b472ee58c958dc
cdomizi <domizicesare@gmail.com>
2025-01-28 16:40:50 +0100

    add qrcode context

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { Footer } from "@layout/Footer";
 import { Navbar } from "@layout/Navbar";
+import { QRCodeProvider } from "./contexts/QRCodeContext";
 import { Home } from "./pages/Home/Home";
 
 function App() {
   return (
-    <>
+    <QRCodeProvider>
       <Navbar />
       <main>
         <Home />
       </main>
       <Footer />
-    </>
+    </QRCodeProvider>
   );
 }
 
---
+++ src/contexts/QRCodeContext.tsx
@@ -0,0 +1,18 @@
+import { createContext, ReactNode, useState } from "react";
+
+type QRCodeContext = {
+  qrcode: string;
+  setQRCode: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const INITIAL_QRCODE = "./placeholder-qrcode.png";
+
+const QRCodeContext = createContext({} as QRCodeContext);
+
+export function QRCodeProvider({ children }: { children: ReactNode }) {
+  const [qrcode, setQRCode] = useState(INITIAL_QRCODE);
+
+  return (
+    <QRCodeContext value={{ qrcode, setQRCode }}>{children}</QRCodeContext>
+  );
+}
