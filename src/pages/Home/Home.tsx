aa1f88ca7317146853f56c3405e47f76ce755888
cdomizi <domizicesare@gmail.com>
2025-01-28 17:55:03 +0100

    make home page responsive

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,10 @@ import { QRCode } from "@/components/QRCode/QRCode";
 
 export function Home() {
   return (
-    <div id="home" className="container mx-auto flex flex-col flex-nowrap">
+    <div
+      id="home"
+      className="container mx-auto flex flex-col flex-nowrap md:flex-row lg:max-w-48"
+    >
       <GeneratorForm />
       <QRCode />
     </div>
