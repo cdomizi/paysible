16580cc01d7ece45618140195a0f11fcfc81289d
cdomizi <domizicesare@gmail.com>
2025-02-05 17:49:37 +0100

    add QR code copyright notice to footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,10 +1,14 @@
-const currentYear = new Date().getFullYear();
+const currentYear = new Date().getFullYear().toString();
 
 export function Footer() {
   return (
     <footer className="footer footer-center mt-auto p-4 text-base-content/50">
       <aside>
         <p>Copyright &copy; {currentYear} - All rights reserved</p>
+        <p className="text-xs">
+          &quot;QR Code&quot; is a registered trademark of DENSO WAVE
+          INCORPORATED
+        </p>
       </aside>
     </footer>
   );
