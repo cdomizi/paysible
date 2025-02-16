6395d451c75a66ed03c55d96a8e6e0d2095044a8
cdomizi <domizicesare@gmail.com>
2025-02-16 20:03:10 +0100

    change default font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 import tailwindTypography from "@tailwindcss/typography";
 import daisyui from "daisyui";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [tailwindTypography, daisyui],
   daisyui: {
